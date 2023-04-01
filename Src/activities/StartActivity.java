2b99986bacc491cf25bec938c49d87a492f959f6
Alin Marin <alin.ma21@yahoo.com>
2023-04-01 16:53:09 +0300

    create an animation for start activity

--- Src/activities/StartActivity.java
+++ Src/activities/StartActivity.java
@@ -1,8 +1,9 @@
 package com.example.smartliving;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+
+        /*start the connectivity animation*/
+        ImageView firstPageConnectivity = findViewById(R.id.firstPageConnectivity);
+        AnimationDrawable connectivityAnimation = (AnimationDrawable) firstPageConnectivity.getDrawable();
+        connectivityAnimation.start();
+
+        /*the animmation will run for 6.6s*/
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        /*after 6.6s, the animation stops and a new activity will start*/
+                        connectivityAnimation.stop();
+
+                    }
+                }, 6600);
     }
 }
