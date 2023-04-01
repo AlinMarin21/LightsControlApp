0c6d03fb47b67290d12ed14596c0030e2e8d661b
Alin Marin <alin.ma21@yahoo.com>
2023-04-01 17:25:49 +0300

    create an async task for bluetooth connectivity

--- Src/activities/StartActivity.java
+++ Src/activities/StartActivity.java
@@ -2,8 +2,10 @@ package com.example.smartliving;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageView;
+
 
 public class StartActivity extends AppCompatActivity {
 
@@ -12,21 +14,55 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        new bluetoothConnectionTask().execute();
 
-        /*start the connectivity animation*/
-        ImageView firstPageConnectivity = findViewById(R.id.firstPageConnectivity);
-        AnimationDrawable connectivityAnimation = (AnimationDrawable) firstPageConnectivity.getDrawable();
-        connectivityAnimation.start();
 
-        /*the animmation will run for 6.6s*/
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        /*after 6.6s, the animation stops and a new activity will start*/
-                        connectivityAnimation.stop();
-
-                    }
-                }, 6600);
+//        /*start the connectivity animation*/
+//        ImageView firstPageConnectivity = findViewById(R.id.firstPageConnectivity);
+//        AnimationDrawable connectivityAnimation = (AnimationDrawable) firstPageConnectivity.getDrawable();
+//        connectivityAnimation.start();
+//
+//        /*the animmation will run for 6.6s*/
+//        new android.os.Handler().postDelayed(
+//                new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        /*after 6.6s, the animation stops and a new activity will start*/
+//                        connectivityAnimation.stop();
+//
+//                    }
+//                }, 6600);
     }
+
+    private class bluetoothConnectionTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            System.out.println("animation start");
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("animation on background");
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            System.out.println("animation stop");
+        }
+    }
+
+
 }
